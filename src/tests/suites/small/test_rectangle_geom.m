a001cd88cbe1988c2f7e5891e5bdd374d8e30baa
Roman Putanowicz <putanowr@gmail.com>
2019-02-08 12:02:48 +0100

    Make verbose GMSH output

--- src/tests/suites/small/test_rectangle_geom.m
+++ src/tests/suites/small/test_rectangle_geom.m
@@ -3,8 +3,8 @@ global mp_TEST
 gmshgeom = mp_geom_rectangle([0,0], [2,1], struct('lc',1.0));
 meshingParams.basename ='rectangle';
 meshingParams.dim = 2;
-meshingParams.folder = mp_TEST.testdir
+meshingParams.folder = mp_TEST.testdir;
+meshingParams.showinfo = true;
+meshingParams.verbosity = 4;
 [status, stdout, stderr] = mp_gmsh(gmshgeom, meshingParams);
-disp(stdout)
-disp(stderr)
 mp_test_assert_equal(true, status)
