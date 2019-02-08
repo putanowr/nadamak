7f95541bf91652a298d01d58b051b11064bb02ba
Roman Putanowicz <putanowr@gmail.com>
2019-02-08 11:50:35 +0100

    Make verbose output for debugging buildserver

--- src/tests/suites/small/test_rectangle_geom.m
+++ src/tests/suites/small/test_rectangle_geom.m
@@ -3,6 +3,6 @@ global mp_TEST
 gmshgeom = mp_geom_rectangle([0,0], [2,1], struct('lc',1.0));
 meshingParams.basename ='rectangle';
 meshingParams.dim = 2;
-meshingParams.folder =  mp_TEST.testdir;
+meshingParams.folder = mp_TEST.testdir
 [status, ~, ~] = mp_gmsh(gmshgeom, meshingParams);
 mp_test_assert_equal(true, status)
