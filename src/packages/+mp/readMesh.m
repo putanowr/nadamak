8a7c1525dfb93165860ec03693743d10766a14c2
Roman Putanowicz <putanowr@gmail.com>
2019-02-12 17:44:15 +0100

    Modified to use object oriented interface.

--- src/packages/+mp/readMesh.m
+++ src/packages/+mp/readMesh.m
@@ -1,6 +1,12 @@
 function [mesh] = readMesh(fid_or_name)
   %% Read given file and returns new Mesh object.
-  [nodes, elements, regions, nodemap] = mp_gmsh_read_mesh(fid_or_name);
+  [fid, needclose] = mp_get_fid(fid_or_name);
+  regions = mp_gmsh_read_regions(fid);
+  [nodes, nodemap] = mp.gmsh.read_nodes(fid);  
+  elements = mp.gmsh.read_elements(fid);
+  if needclose
+    fclose(fid);
+  end
   elemInfo = mp_gmsh_elems_info(elements);
   mesh = mp.Mesh(elemInfo.dim, nodes, elements, regions, nodemap);
 end
