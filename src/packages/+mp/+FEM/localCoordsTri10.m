884adc9b77d851697b2eb84691db6dbfd06739d4
Roman Putanowicz <putanowr@gmail.com>
2019-02-19 11:34:18 +0100

    Added implementation of Triang10 finite element.

---
+++ src/packages/+mp/+FEM/localCoordsTri10.m
@@ -0,0 +1,15 @@
+function coords = localCoordsTri10(dofID)
+  % Normalized coordinates in reference element corresponding to DOF location.
+  % If no dofID is given return the whole array of coordinates. 
+  persistent crds 
+  if isempty(crds)
+    info = mp_gmsh_types_info('type_21');
+    crds = info.nodes;
+  end
+  if nargin < 1 
+    coords = crds;
+  else
+    r = rem(dofID-1,10)+1;
+    coords = crds(r, :);
+  end
+end
