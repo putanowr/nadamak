efcf1c70733ac0bc4a19d93db9524955daa7f04c
Roman Putanowicz <putanowr@gmail.com>
2019-02-20 23:30:56 +0100

    Added handle to shape functions

--- src/packages/+mp/+FEM/FemType.m
+++ src/packages/+mp/+FEM/FemType.m
@@ -3,20 +3,21 @@ classdef FemType
   enumeration
     % items: gmsh_elem_type, isLagrangian, order, num_of_dofs, 
     % localCoordsCalback, dofTopoCallback
-    Line2( 1,true,1, 2, @mp.FEM.localCoordsLine2, @mp.FEM.dofTopoLine2)
-    Triang3(  2,true,1, 3, @mp.FEM.localCoordsTriang3,  @mp.FEM.dofTopoTriang3)
-    Triang6(  9,true,2, 6, @mp.FEM.localCoordsTriang6,  @mp.FEM.dofTopoTriang6)
-    Triang10(21,true,3,10, @mp.FEM.localCoordsTriang10, @mp.FEM.dofTopoTriang10)
-    Quad4( 3,true,1, 4, @mp.FEM.localCoordsQuad4, @mp.FEM.dofTopoQuad4)
+    Line2( 1,true,1, 2, @mp.FEM.localCoordsLine2, @mp.FEM.dofTopoLine2, @mp.FEM.sfLine2)
+    Triang3(  2,true,1, 3, @mp.FEM.localCoordsTriang3,  @mp.FEM.dofTopoTriang3, @mp.FEM.sfTriang3)
+    Triang6(  9,true,2, 6, @mp.FEM.localCoordsTriang6,  @mp.FEM.dofTopoTriang6, @mp.FEM.sfTriang6)
+    Triang10(21,true,3,10, @mp.FEM.localCoordsTriang10, @mp.FEM.dofTopoTriang10, @mp.FEM.sfTriang10)
+    Quad4( 3,true,1, 4, @mp.FEM.localCoordsQuad4, @mp.FEM.dofTopoQuad4, @mp.FEM.sfQuad4)
   end
   methods
-    function [self]=FemType(gmshID, isLagrangian, order, numOfDofs, cfh, tfh)
+    function [self]=FemType(gmshID, isLagrangian, order, numOfDofs, cfh, tfh, sfh)
       self.gmshID = gmshID;
       self.isLagrangian = isLagrangian;
       self.order = order;
       self.numOfDofs = numOfDofs;
       self.cfh = cfh; 
-      self.tfh = tfh; 
+      self.tfh = tfh;
+      self.sfh = sfh;
     end
     function [info]=getInfo(self)
       info = mp_gmsh_types_info(self.gmshID);
@@ -49,5 +50,6 @@ classdef FemType
     numOfDofs
     cfh
     tfh
+    sfh
   end
 end
