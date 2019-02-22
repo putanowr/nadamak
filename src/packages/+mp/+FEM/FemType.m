a2477e4789ee4ba8600eecfce4bade9df5d36da6
Roman Putanowicz <putanowr@gmail.com>
2019-02-22 12:37:46 +0100

    Added dimension to the parameters of FemType.

--- src/packages/+mp/+FEM/FemType.m
+++ src/packages/+mp/+FEM/FemType.m
@@ -3,14 +3,15 @@ classdef FemType
   enumeration
     % items: gmsh_elem_type, isLagrangian, order, num_of_dofs, 
     % localCoordsCalback, dofTopoCallback
-    Line2( 1,true,1, 2, @mp.FEM.localCoordsLine2, @mp.FEM.dofTopoLine2, @mp.FEM.sfLine2)
-    Triang3(  2,true,1, 3, @mp.FEM.localCoordsTriang3,  @mp.FEM.dofTopoTriang3, @mp.FEM.sfTriang3)
-    Triang6(  9,true,2, 6, @mp.FEM.localCoordsTriang6,  @mp.FEM.dofTopoTriang6, @mp.FEM.sfTriang6)
-    Triang10(21,true,3,10, @mp.FEM.localCoordsTriang10, @mp.FEM.dofTopoTriang10, @mp.FEM.sfTriang10)
-    Quad4( 3,true,1, 4, @mp.FEM.localCoordsQuad4, @mp.FEM.dofTopoQuad4, @mp.FEM.sfQuad4)
+    Line2( 1,true,1,1, 2, @mp.FEM.localCoordsLine2, @mp.FEM.dofTopoLine2, @mp.FEM.sfLine2)
+    Triang3(  2,true,2,1, 3, @mp.FEM.localCoordsTriang3,  @mp.FEM.dofTopoTriang3, @mp.FEM.sfTriang3)
+    Triang6(  9,true,2,2, 6, @mp.FEM.localCoordsTriang6,  @mp.FEM.dofTopoTriang6, @mp.FEM.sfTriang6)
+    Triang10(21,true,2,3,10, @mp.FEM.localCoordsTriang10, @mp.FEM.dofTopoTriang10, @mp.FEM.sfTriang10)
+    Quad4( 3,true,2,1, 4, @mp.FEM.localCoordsQuad4, @mp.FEM.dofTopoQuad4, @mp.FEM.sfQuad4)
   end
   methods
-    function [self]=FemType(gmshID, isLagrangian, order, numOfDofs, cfh, tfh, sfh)
+    function [self]=FemType(gmshID, isLagrangian, dim, order, numOfDofs, cfh, tfh, sfh)
+      self.dim = dim;
       self.gmshID = gmshID;
       self.isLagrangian = isLagrangian;
       self.order = order;
@@ -44,6 +45,7 @@ classdef FemType
     end
   end 
   properties(SetAccess=immutable)
+    dim
     gmshID
     isLagrangian
     order
