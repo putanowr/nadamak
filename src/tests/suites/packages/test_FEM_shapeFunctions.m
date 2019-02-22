c7aca2b30a5ca1b61c1cc2ab649efbcf0f9488e0
Roman Putanowicz <putanowr@gmail.com>
2019-02-22 13:37:12 +0100

    Added unit test for FEM shape functions.

---
+++ src/tests/suites/packages/test_FEM_shapeFunctions.m
@@ -0,0 +1,13 @@
+mp_test_register('FEM_shapeFunctions', 'Testing FEM shape functions')
+
+for fem = enumeration('mp.FEM.FemType')'
+  details = sprintf('Check Fem type: %s', fem);
+  xy = fem.dofsCoords();
+
+  N = fem.sfh(xy);
+  I = eye(size(N));
+  check = norm(N-I, 'fro');
+  mp_test_assert_equal_double(0.0, check, 1e-5, details); 
+end
+
+mp_manage_demos('report', 'FEM_shapeFunctions', true);
