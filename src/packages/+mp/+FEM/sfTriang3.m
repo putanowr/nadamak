a895fc8e6407ec9ee05b63aae9b4925c9cecf9f7
Roman Putanowicz <putanowr@gmail.com>
2019-02-24 08:03:00 +0100

    Removed functions that are qutomatically generated.

--- src/packages/+mp/+FEM/sfTriang3.m
+++
@@ -1,6 +0,0 @@
-function Nb = sfTriang3(refPoint)
-  % Shape functions for classical linear Lagrangian triangle element
-  x = refPoint(:,1);
-  y = refPoint(:,2);
-  Nb = [1-x-y, x, y];
-end 
--- src/packages/+mp/+FEM/sfTriang6.m
+++
@@ -1,8 +0,0 @@
-function Nb = sfTriang6(refPoint)
-% Shape functions for classical quadratic Lagrangian triangle element
-% It is assumed that reference element is in normalized coordinates.
-  x = refPoint(:,1);
-  y = refPoint(:,2);
-  z = 1 - x - y;
-  Nb = [z.*(z-y-x), x.*(x-z-y), y.*(y-x-z),4*x.*z,4*y.*x,4*y.*z];
-end
