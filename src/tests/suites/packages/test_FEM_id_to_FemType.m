dcd352b114e723687051e3429071a9641fa152dc
Roman Putanowicz <putanowr@gmail.com>
2019-02-24 23:54:50 +0100

    Fixed typo

--- src/tests/suites/packages/test_FEM_id_to_FemType.m
+++ src/tests/suites/packages/test_FEM_id_to_FemType.m
@@ -1,4 +1,4 @@
 mp_test_register('FEM_id_to_FemType', 'Testing convertion int -> FemType')
 
-fem = mp.Fem.FemType.fromId(1);
+fem = mp.FEM.FemType.fromId(1);
 mp_test_assert_equal(mp.FEM.FemType.Line2, fem)
