15d83bae3f515e629f3478ae99c3671d68e6bc6a
Roman Putanowicz <putanowr@gmail.com>
2019-02-24 23:58:27 +0100

    Simplify GeomTrans by using convertion between gmsh elemen id and FemType. Store in GeomTrans FemType instead of transformation function handle.

--- src/packages/+mp/GeomTrans.m
+++ src/packages/+mp/GeomTrans.m
@@ -2,7 +2,7 @@ classdef GeomTrans < handle
   %GeomTrans - performs geometric transformation from reference to real element. 
   properties (Access=private)
     mesh
-    sfEvaluator  
+    fem  
     cellToNodes 
   end
   methods
@@ -15,27 +15,14 @@ classdef GeomTrans < handle
         %error('GeomTrans not cannot handle mixed element type meshes yet')
         return
       end 
-      switch ct(1)
-        case 1
-           obj.sfEvaluator = @mp.FEM.sfLine2;
-        case 2
-           obj.sfEvaluator = @mp.FEM.sfTriang3;
-        case 9
-           obj.sfEvaluator = @mp.FEM.sfTriang6;
-        case 3
-           obj.sfEvaluator = @mp.FEM.sfQuad4;  
-	case 21
-	   obj.sfEvaluator = @mp.FEM.sfTriang10;	 
-        otherwise
-          error('GeomTrans cannot handle element type %d', ct(1))
+      obj.fem = mp.FEM.FemType.fromId(ct(1));
       end
     end
     function [xyz] = transform(obj, refPoint, cellID)
       % Return real coordinates for point with reference coordinates in given element
       cellNodes = obj.cellToNodes.at(cellID);
       refXYZ = obj.mesh.nodes(cellNodes, :);
-      shapeFun = obj.sfEvaluator;
-      sf = shapeFun(refPoint);
+      sf = fem.sfh(refPoint);
       xyz = [dot(sf, refXYZ(:,1)), dot(sf, refXYZ(:,2)), dot(sf, refXYZ(:,3))]; 
     end
   end
