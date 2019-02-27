06e97e522913351977e3f22f5dffe4dba8150ca9
Roman Putanowicz <putanowr@gmail.com>
2019-02-27 05:39:41 +0100

    Added method GeomTrans.jacobian to calculate Jacobian matrix.

--- src/packages/+mp/GeomTrans.m
+++ src/packages/+mp/GeomTrans.m
@@ -1,9 +1,9 @@
 classdef GeomTrans < handle
-  %GeomTrans - performs geometric transformation from reference to real element. 
+  %GeomTrans - performs geometric transformation from reference to real element.
   properties (Access=private)
     mesh
-    fem  
-    cellToNodes 
+    fem
+    cellToNodes
   end
   methods
     function [obj] = GeomTrans(mesh)
@@ -11,10 +11,10 @@ classdef GeomTrans < handle
       dim = mesh.dim;
       obj.cellToNodes = mesh.getAdjacency(dim, 0);
       ct = mesh.cellTypes();
-      if (length(ct) > 1) 
+      if (length(ct) > 1)
         %error('GeomTrans not cannot handle mixed element type meshes yet')
         return
-      end 
+      end
       obj.fem = mp.FEM.FemType.fromId(ct(1));
     end
     function [xyz] = transform(obj, refPoint, cellID)
@@ -22,7 +22,25 @@ classdef GeomTrans < handle
       cellNodes = obj.cellToNodes.at(cellID);
       refXYZ = obj.mesh.nodes(cellNodes, :);
       sf = obj.fem.sfh(refPoint);
-      xyz = [sf*refXYZ(:,1), sf*refXYZ(:,2), sf*refXYZ(:,3)]; 
+      xyz = [sf*refXYZ(:,1), sf*refXYZ(:,2), sf*refXYZ(:,3)];
+    end
+    function [jmat] = jacobian(obj, refPts, cellID, displacement)
+      % calcualte jacobian matrix of the geometric transformation at reference
+      % points of given cell.
+      cellNodes = obj.cellToNodes.at(cellID);
+      refXYZ = obj.mesh.nodes(cellNodes, :);
+      n = size(displacement,2);
+      if nargin > 3
+        refXYZ(:, 1:n) = refXYZ(:, 1:n) + displacement(cellNodes, :);
+      end
+      npt = size(refPts, 1);
+      d = obj.mesh.dim;
+      jmat = zeros(d, d, npt);
+      k = 1;
+      for k = 1:size(refPts, 1);
+        sfD = obj.fem.sfDh(refPts(k,:));
+        jmat(:,:,k) = sfD*refXYZ(:, 1:d);
+      end
     end
   end
 end
