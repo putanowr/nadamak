9be765097d0d123aa9947d2f344395ffad5e1555
Roman Putanowicz <putanowr@gmail.com>
2019-03-08 11:35:39 +0100

    Sorting enums alphabetically

--- src/tests/suites/packages/test_ProblemFactory.m
+++ src/tests/suites/packages/test_ProblemFactory.m
@@ -3,6 +3,8 @@ kind = {{'M',  mp.ProblemType.Mechanical}, ...
         {'T',  mp.ProblemType.Thermal}, ...
         {'TM', mp.ProblemType.ThermoMechanical}};
 for  k = kind
-  problem = mp.ProblemFactory.produce(k{1}(1));
+  problem = mp.ProblemFactory.produce(k{1}(1))
+  problem.type
+  k{1}(2)
   mp_test_assert_equal(k{1}(2), problem.type);
 end
