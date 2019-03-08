902ac5de9fc972274b332db19682f3f785f608a8
Roman Putanowicz <putanowr@gmail.com>
2019-03-08 11:28:36 +0100

    Fixed line continuation making it explicit

--- src/tests/suites/packages/test_ProblemFactory.m
+++ src/tests/suites/packages/test_ProblemFactory.m
@@ -1,6 +1,6 @@
 mp_test_register('producere_Problems', 'Testing ProblemFactory producer')
-kind = {{'M',  mp.ProblemType.Mechanical},
-        {'T',  mp.ProblemType.Thermal},
+kind = {{'M',  mp.ProblemType.Mechanical}, ...
+        {'T',  mp.ProblemType.Thermal}, ...
         {'TM', mp.ProblemType.ThermoMechanical}};
 for  k = kind
   problem = mp.ProblemFactory.produce(k{1}(1));
