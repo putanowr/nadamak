24c292ec8b747f2bd8822c60ff33765dcac87f6a
Roman Putanowicz <putanowr@gmail.com>
2019-03-08 11:43:12 +0100

    Fixed access to cell array element

--- src/tests/suites/packages/test_ProblemFactory.m
+++ src/tests/suites/packages/test_ProblemFactory.m
@@ -3,8 +3,6 @@ kind = {{'M',  mp.ProblemType.Mechanical}, ...
         {'T',  mp.ProblemType.Thermal}, ...
         {'TM', mp.ProblemType.ThermoMechanical}};
 for  k = kind
-  problem = mp.ProblemFactory.produce(k{1}(1))
-  problem.type
-  k{1}(2)
-  mp_test_assert_equal(k{1}(2), problem.type);
+  problem = mp.ProblemFactory.produce(k{1}(1));
+  mp_test_assert_equal(k{1}{2}, problem.type);
 end
