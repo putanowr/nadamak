13339b2106e0e5dc7a26fa210bd9c1457f578583
Roman Putanowicz <putanowr@gmail.com>
2019-03-08 05:35:08 +0100

    Added unit test to ProblemFactory producer.

---
+++ src/tests/suites/packages/test_ProblemFactory.m
@@ -0,0 +1,8 @@
+mp_test_register('producere_Problems', 'Testing ProblemFactory producer')
+kind = {{'M',  mp.ProblemType.Mechanical},
+        {'T',  mp.ProblemType.Thermal},
+        {'TM', mp.ProblemType.ThermoMechanical}};
+for  k = kind
+  problem = mp.ProblemFactory.produce(k{1})(1)};
+  mp_test_assert_equal(k{1}(2), problem.type);
+end
