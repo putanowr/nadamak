1a24fa122bca085b6bf75ec40d05faa784c1cee8
Roman Putanowicz <putanowr@gmail.com>
2019-03-09 08:24:16 +0100

    Added test for Mesh read and write formats info.

---
+++ src/tests/suites/packages/test_Mesh_formats.m
@@ -0,0 +1,9 @@
+mp_test_register('Mesh_formats', 'Testing Mesh I/O formats info')
+
+for format = {'gmsh'}
+  msg = sprintf('Failed to see format: "%s"', format);
+  status = contains(mp.Mesh.readFormats, format);
+  mp_test_assert(status, msg); 
+  status = contains(mp.Mesh.writeFormats, format);
+  mp_test_assert(status, msg);
+end
