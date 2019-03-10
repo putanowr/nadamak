85111aa272422e07e326cfe4e44ab329486256f7
Roman Putanowicz <putanowr@gmail.com>
2019-03-10 11:39:49 +0100

    Added Progress as a property of Problem. Removed progress argument from Problem methods.

--- src/packages/+mp/Problem.m
+++ src/packages/+mp/Problem.m
@@ -4,6 +4,7 @@ classdef Problem < handle
     type mp.ProblemType;
     model mp.FemModel;
     geometry;
+    progress mp.Progress;
   end
   methods
     function [obj] = Problem(type_, geometry)
@@ -11,6 +12,10 @@ classdef Problem < handle
       if nargin > 1 
         obj.setGeometry(geometry);
       end  
+      obj.progress = Progress();
+    end
+    function setProgressReporter(obj, reporter)
+      obj.progress = reporter;
     end
     function setGeometry(obj, geometry)
       if ischar(geometry)
@@ -19,27 +24,27 @@ classdef Problem < handle
         obj.geometry = geometry;
       end
     end
-    function solve(obj, progress)
-      obj.buildMeshes(progress);
-      obj.preassembly(progress);
-      obj.assembly(progress)
-      obj.runSolver(progress);
-      obj.postprocess(progress);
+    function solve(obj)
+      obj.buildMeshes();
+      obj.preassembly();
+      obj.assembly()
+      obj.runSolver();
+      obj.postprocess();
     end
-    function buildMeshes(obj, progress)
-      progress.report(0.1, 'Building meshes');
+    function buildMeshes(obj)
+      obj.progress.report(0.1, 'Building meshes');
     end
-    function preassembly(obj, progress)
-      progress.report(0.2, 'Do pre-assembly');
+    function preassembly(obj)
+      obj.progress.report(0.2, 'Do pre-assembly');
     end
-    function assembly(obj, progress)
-      progress.report(0.3, 'Assembly');
+    function assembly(obj)
+      obj.progress.report(0.3, 'Assembly');
     end
-    function runSolver(obj, progress)
-      progress.report(0.6, 'Run solver');
+    function runSolver(obj)
+      obj.progress.report(0.6, 'Run solver');
     end
-    function postprocess(obj, progress)
-      progress.report(0.8, 'Postprocess');
+    function postprocess(obj)
+      obj.progress.report(0.8, 'Postprocess');
     end  
   end
 end
