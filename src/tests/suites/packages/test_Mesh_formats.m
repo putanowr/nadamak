c5c711102baceaf2e6564452c76b0de23565f97d
Roman Putanowicz <putanowr@gmail.com>
2019-03-10 13:06:29 +0100

    Fixed bug : for iteration a row cell matrix is required.

--- src/tests/suites/packages/test_Mesh_formats.m
+++ src/tests/suites/packages/test_Mesh_formats.m
@@ -1,6 +1,6 @@
 mp_test_register('Mesh_formats', 'Testing Mesh I/O formats info')
 
-for format = {'msh'}
+for format = {'msh'}'
   msg = sprintf('Failed to see format: "%s"', format);
   status = contains(mp.Mesh.readFormats, format);
   mp_test_assert(status, msg); 
