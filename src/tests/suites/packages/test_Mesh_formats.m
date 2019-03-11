e780200ccf6d82bfc6bbfdadfcb718491b1d4530
Roman Putanowicz <putanowr@gmail.com>
2019-03-11 11:01:31 +0100

    Fixed bug in accessing cell content.

--- src/tests/suites/packages/test_Mesh_formats.m
+++ src/tests/suites/packages/test_Mesh_formats.m
@@ -1,7 +1,7 @@
 mp_test_register('Mesh_formats', 'Testing Mesh I/O formats info')
 
 for format = {'msh'}'
-  msg = sprintf('Failed to see format: "%s"', format);
+  msg = sprintf('Failed to see format: "%s"', format{:});
   status = contains(mp.Mesh.readFormats, format);
   mp_test_assert(status, msg); 
   status = contains(mp.Mesh.writeFormats, format);
