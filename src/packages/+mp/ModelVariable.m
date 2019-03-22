50fc2dfe36c32119db08418fcd4f06bde6d55fe2
Roman Putanowicz <putanowr@gmail.com>
2019-03-22 17:30:13 +0100

    Renamed property meshFem -> fem. Added offset property

--- src/packages/+mp/ModelVariable.m
+++ src/packages/+mp/ModelVariable.m
@@ -2,28 +2,33 @@ classdef ModelVariable < handle
   % GeomModel Represents geometric model.
   properties(SetAccess=protected)
     variable
-    meshFem
+    fem
     dofs
+    offset
   end
   methods
     function [obj] = ModelVariable(variable, params)
       obj.variable = variable;
+      obj.offset = 0;
       if isfield(params, 'fem')
-        obj.meshFem = params.('fem');
+        obj.fem = params.('fem');
       else
-        obj.meshFem = [];
+        obj.fem = [];
       end
       if ~isempty(obj.fem)
-        dofs = zeros(obj.variable.numOfComponents, obj.fem.numOfDofs);
+        obj.dofs = zeros(obj.variable.numOfComponents, obj.fem.numOfDofs);
       else
-        dofs = zeros(obj.variable.numOfComponents, 1);
+        obj.dofs = zeros(obj.variable.numOfComponents, 1);
       end
     end
-    function [geomgmsh] = numOfDofs(obj)
-      ndf = size(obj.dofs,1)*size(obj.dofs,2);
+    function setOffset(obj, offset)
+      obj.offset = offset;
+    end
+    function [ndf] = numOfDofs(obj)
+      ndf = obj.fem.numOfDofs;
     end  
     function status = isFem(obj)
-      status = ~isempty(obj.meshFem);
+      status = ~isempty(obj.fem);
     end
   end
 end
