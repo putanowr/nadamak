ac2cb279f85484d437d875fd4581625ae1abbace
Roman Putanowicz <putanowr@gmail.com>
2019-03-22 17:33:05 +0100

    Added property to hold current offset for new variables. Implemented method to add model variable to registry

--- src/packages/+mp/VariableRegistry.m
+++ src/packages/+mp/VariableRegistry.m
@@ -2,12 +2,24 @@ classdef VariableRegistry < handle
   % Manages variables necessary for a Problem
   properties (Access=private)
     vd struct % Structure with fields corresponding to variable names.
+    offset uint32
   end
   methods
     function [obj] = VariableRegistry()
       obj.vd = struct();
+      obj.offset = 0;
     end
-    function addFEMVariable(obj, variableName, fem);
+    function createVariable(obj, variableName, fem)
+    end
+    function addVariable(obj, modelVariable)
+      if modelVariable.isFem()
+        ndof = modelVariable.fem.enumerateDofs();
+      else
+        ndof = prod(modelVariable.qdim);
+      end
+      modelVariable.setOffset(obj.offset);
+      obj.vd.(modelVariable.variable.name) = modelVariable;
+      obj.offset = obj.offset+ndof;
     end
   end
 end
