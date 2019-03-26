7593616631a59d59dee12a690497938723eb505e
Roman Putanowicz <putanowr@gmail.com>
2019-03-26 09:47:33 +0100

    Implemented Cube geometry.

--- src/packages/+mp/+geoms/CubeGeom.m
+++ src/packages/+mp/+geoms/CubeGeom.m
@@ -1,20 +1,27 @@
-classdef CubeGeom < mp.GeomModel 
+classdef CubeGeom < mp.GeomModel
   % Geometric model for CubeGeom.
+  properties (Access=public)
+    params = struct();
+  end
   methods
     function [obj] = CubeGeom(name, params, legacyID)
       if nargin < 3
         legacyID = 0;
-      end  
+      end
       obj = obj@mp.GeomModel(name, 3, legacyID);
     end
     function [name] = templateName(~)
-      name = 'not_implemented.tpl';
+      name = 'cube.tpl';
     end
     function [geomgmsh] = as_gmsh(obj)
-      geomgmsh = 'not mimplented';
+      tpl = fileread(fullfile(obj.templateDir, obj.templateName()));
+      geomgmsh = mp_tpl_substitute(tpl, obj.params);
     end
     function [maxlc] = coarsest_lc(obj)
-      error('not implemented');
+      maxlc = 1.0;
+    end
+    function [regionNames] = regions(~)
+      regionNames = {'d_cube', 'b_top', 'b_bottom', 'b_left', 'b_right', 'b_front', 'b_back'};
     end
   end
 end
