2a3d9ed6f25868f9c464977f1c18ff89db680f98
Roman Putanowicz <putanowr@gmail.com>
2019-03-29 14:14:43 +0100

    Fixed bugs

--- src/packages/+mp/+IM/GaussProduct2D.m
+++ src/packages/+mp/+IM/GaussProduct2D.m
@@ -1,12 +1,12 @@
 classdef GaussProduct2D < mp.IM.Product2D
   methods
     function [obj] = GaussProduct2D(order)
-      quad = Gauss1D(order);
-      obj = mp.IM.Product2D(quad, quad);
+      qd = mp.IM.Gauss1D(order);
+      obj = obj@mp.IM.Product2D(qd, qd);
       obj.baseType = mp.IM.ImType.GaussProduct2D;
     end
-  end 
-  function [name] = getfem_name(obj)
-    name = sprintf('IM_GAUSS_PARALLELEPIPED(2,%d)',obj.order(1));
+    function [name] = getfem_name(obj)
+      name = sprintf('IM_GAUSS_PARALLELEPIPED(2,%d)',obj.order(1));
+    end
   end
 end
