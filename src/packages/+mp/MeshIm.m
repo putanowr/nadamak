c0e6d6884d889603b3e4212aebc225ac0b595268
Roman Putanowicz <putanowr@gmail.com>
2019-04-01 11:20:15 +0200

    Added class to handle integration over meshes

---
+++ src/packages/+mp/MeshIm.m
@@ -0,0 +1,32 @@
+classdef MeshIm < handle
+  % MeshIm holds association between mesh elements and integration rules.
+  properties (SetAccess=private)
+    mesh % reference to the underlying mesh
+    region % region ID of the mesh
+    quadratures
+    elemType2quad
+  end
+  methods
+    function [obj] = MeshIm(mesh, region, order)
+      obj.mesh = mesh;
+      obj.region = region;
+      obj.quadratures = cell();
+      obj.elemType2quad = containers.Map('KeyType', 'int32', ...
+                                         'ValueType', 'int32');
+      obj.setup(order);
+    end
+  end
+  methods(Access=private)
+    function setup(obj, order)
+      nelem = obj.mesh.elemsCount();
+      for i=1:nelem
+        et = obj.mesh.elementGmshType(i);
+        if ~obj.elemType2quad.isKey(et)
+          obj.quadratres{end} = mp.IM.QuadratureFactory(et, order);
+          obj.elemType2quad(et) = numel(obj.quadratures);
+        end
+      end
+    end
+  end
+end  
+
