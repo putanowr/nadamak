32c0be4825a7de2d00bb23e6e3e38d9b0851da87
Roman Putanowicz <putanowr@L5.pk.edu.pl>
2019-04-02 21:32:16 +0100

    Updated guid autors

--- docs/guide/en/source/conf.py
+++ docs/guide/en/source/conf.py
@@ -59,7 +59,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Nadamak'
-copyright = u'2019, Roman Putanowicz, Sławomi Milewski'
+copyright = u'2019, Roman Putanowicz'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
