839c69438d754870dc23a6334dbde75f4c42dc3d
Roman Putanowicz <putanowr@gmail.com>
2019-04-03 21:19:13 +0200

    Restored undersore prefix in folders

--- docs/guide/en/source/conf.py
+++ docs/guide/en/source/conf.py
@@ -46,7 +46,7 @@ matlab_src_dir = os.path.abspath('../../../../src/')
 print(matlab_src_dir)
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -142,7 +142,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
