a3b939ac7263f47ce1ed3bb38e39ff4415157a36
Roman Putanowicz <putanowr@gmail.com>
2019-04-05 11:15:04 +0200

    Added Line geometry

---
+++ src/packages/+mp/+geoms/LineGeom.m
@@ -0,0 +1,42 @@
+classdef LineGeom < mp.GeomModel 
+  % Geometric model of a rectangle.
+  properties (Access = public)
+    params = struct('dL', 1.0, 'lcFactors', [1,1]);
+  end
+  methods (Access=private)
+    function setup(obj, params_)
+      obj.params.dL = mp_get_option(params_, 'dL', obj.params.dL);
+      obj.params.lcFactors = mp_get_option(params_, 'lcFactors', obj.params.lcFactors);
+    end
+  end
+  methods
+    function [obj] = LineGeom(name, params_, legacyID)
+      if nargin < 3
+        legacyID = 10;
+      end
+      if nargin < 2
+        params_ = struct();
+      end
+      obj = obj@mp.GeomModel(name, 1, 2, legacyID);
+      if ~isempty(params_)
+        obj.setup(params_);
+      end
+    end
+    function [name] = templateName(~)
+      name = 'line.tpl';
+    end
+    function [regionNames] = regions(~)
+      regionNames = {'d_domain', 'p_endpoints'};
+    end
+    function [geomgmsh] = as_gmsh(obj)
+      prms = obj.params;
+      prms.pt1 = [0,0,0];
+      prms.pt2 = [prms.dL,0,0];
+      tpl = fileread(fullfile(obj.templateDir, obj.templateName()));
+      geomgmsh = mp_tpl_substitute(tpl, prms);
+    end
+    function [maxlc] = coarsest_lc(obj)
+      maxlc = max(obj.params.da, obj.params.db);
+    end
+  end
+end
