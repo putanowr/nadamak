6bd645463fe67bec23d13be34fd3a5b60dccf06b
Roman Putanowicz <putanowr@gmail.com>
2019-05-25 22:16:01 +0200

    Implemented volumeForm method. Fixed naming. Added documentation

--- src/packages/+mp/GeomTrans.m
+++ src/packages/+mp/GeomTrans.m
@@ -5,16 +5,18 @@ classdef GeomTrans < handle
     fem
     cellToNodes
     dim
+    ambientDim
   end
   methods
-    function [obj] = GeomTrans(mesh, targetDim)
+    function [obj] = GeomTrans(mesh, refDim)
       if nargin < 2
         obj.dim = mesh.dim;
       else
-        obj.dim = targetDim;
+        obj.dim = refDim;
       end
-      if (obj.dim > mesh.dim || obj.dim < 1)
-        error('Invalid GeomTrans dimension %d, expected value in range [1, %d]', obj.dim, mesh.dim)
+      obj.ambientDim = mesh.ambientDim;
+      if (obj.dim > obj.ambientDim || obj.dim < 1)
+        error('Invalid GeomTrans dimension %d, expected value in range [1, %d]', obj.dim, obj.ambientDim)
       end
       obj.mesh = mesh;
       obj.cellToNodes = mesh.getAdjacency(obj.dim, 0);
@@ -28,43 +30,48 @@ classdef GeomTrans < handle
     function [xyz] = transform(obj, refPoint, cellID)
       % Return real coordinates for point with reference coordinates in given element
       cellNodes = obj.cellToNodes.at(cellID);
-      refXYZ = obj.mesh.nodes(cellNodes, :);
+      ambientXYZ = obj.mesh.nodes(cellNodes, :);
       sf = obj.fem.sfh(refPoint);
-      xyz = [sf*refXYZ(:,1), sf*refXYZ(:,2), sf*refXYZ(:,3)];
+      xyz = sf*ambientXYZ;
     end
     function [jmat] = jacobianMatrix(obj, refPts, cellID, displacement)
-      % calcualte jacobian matrix of the geometric transformation at reference
+      % Calcualte Jacobian matrix of the geometric transformation at reference
       % points of given cell.
+      % The calculated matrix is 3D. If J_ijk is calculated Jacobina matrix
+      % then index i corresponds to component of the transformation, index
+      % j corresponds to the variable over which derivative is take and
+      % index k corresponds to the point at which Jacobina matrix is
+      % calcualted.
+      % Ths size of J is [ambientDim, dim, size(refPts, 1)].
+      % The slices along k axis give the traditional Jacobina matrix
+      %    df_1/dx_1, df_1/dx2, ..., df_1/dx_k
+      %    ...
+      %    df_n/dx_1, df_n/dx2, ..., df_n/dx_k
+      %
       cellNodes = obj.cellToNodes.at(cellID);
-      refXYZ = obj.mesh.nodes(cellNodes, :);
+      ambientXYZ = obj.mesh.nodes(cellNodes, :);
       n = size(displacement,2);
       if nargin > 3
-        refXYZ(:, 1:n) = refXYZ(:, 1:n) + displacement(cellNodes, :);
+        ambientXYZ(:, 1:n) = ambientXYZ(:, 1:n) + displacement(cellNodes, :);
       end
       npt = size(refPts, 1);
-      d = obj.mesh.dim;
-      jmat = zeros(d, obj.dim, npt);
+      jmat = zeros(obj.ambientDim, obj.dim, npt);
       for k = 1:size(refPts, 1)
         sfD = obj.fem.sfDh(refPts(k,:));
-        jmat(:,:,k) = sfD*refXYZ(:, 1:obj.dim);
+        jmat(1:obj.dim,:,k) = sfD*ambientXYZ(:, 1:obj.dim);
       end
     end
-    function [jac] = jacobian(obj, refPts, cellID, displacement)
-      % calcualte jacobian of the geometric transformation
-      jmat = obj.jacobianMatrix(obj, refPts, cellID, displacement)
+    function [form] = volumeForm(obj, refPts, cellID, displacement)
+      % Calculate pulback of volume form under displacement map
+      % onto reference element.
+      jmat = obj.jacobianMatrix(refPts, cellID, displacement)
       n = size(jmat, 3);
-      jac = zeros(1, n);
-      for i=1:jmat
-        jac = obj.jacobian1D(jmat(:,:, i));
+      form = zeros(1, n);
+      for i=1:n
+        df = jmat(:,:, i);
+        form(i) = sqrt(det(df'*df));
       end
-    end
-  end
-  methods(Access=private)
-    function [d] = jacobian1D(jmat)
-      d = sqrt(sum(jmat.*jmat));
-    end
-    function [d] = jacobian2D(jmat)
-
     end
   end
 end
+
