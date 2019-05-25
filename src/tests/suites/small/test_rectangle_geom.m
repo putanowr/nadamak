438e8db75d0fcbaf2b517648ce777508c230b675
Roman Putanowicz <putanowr@gmail.com>
2019-05-25 21:51:33 +0200

    Silenced unwanted output

--- src/tests/suites/small/test_rectangle_geom.m
+++ src/tests/suites/small/test_rectangle_geom.m
@@ -4,7 +4,6 @@ gmshgeom = mp_geom_rectangle([0,0], [2,1], struct('lc',1.0));
 meshingParams.basename ='rectangle';
 meshingParams.dim = 2;
 meshingParams.folder = mp_TEST.testdir;
-meshingParams.showinfo = true;
 meshingParams.verbosity = 4;
 [status, stdout, stderr] = mp_gmsh(gmshgeom, meshingParams);
 mp_test_assert_equal(true, status)
