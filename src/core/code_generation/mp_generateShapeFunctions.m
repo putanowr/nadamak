75b9b79cfc4ee06382bbf4bf695b77e6658fbab2
Roman Putanowicz <putanowr@gmail.com>
2019-06-17 06:02:30 +0200

    Alternative implementation of shape functions on triangle 6

--- src/core/code_generation/mp_generateShapeFunctions.m
+++ src/core/code_generation/mp_generateShapeFunctions.m
@@ -19,7 +19,8 @@ function status = mp_generateShapeFunctions(pth, verbose)
     sfDefs.Quad9.sf = Quad9ShapeFun();
     sfDefs.Quad8.sf = Quad8ShapeFun();
     sfDefs.Triang3.sf = [z, x, y];
-    sfDefs.Triang6.sf = [z.*(z-y-x), x.*(x-z-y), y.*(y-x-z),4*x.*z,4*y.*x,4*y.*z];
+    sfDefs.Triang6.sf = [z.*(z-y-x), x.*(x-z-y), y.*(y-x-z),4*x.*z,4*y.*x,4*y.*z]
+    %sfDefs.Triang6.sf = Triang6ShapeFun()
     sf = sym('sf', [1,10]);
     sf(1)  = z.*(3*z - 1).*(3*z - 2)/2;
     sf(2)  = x.*(3*x - 1).*(3*x - 2)/2;
@@ -43,7 +44,25 @@ function status = mp_generateShapeFunctions(pth, verbose)
     end
     status = true;
 end
-
+function [sf] = Triang6ShapeFun()
+  syms x y z
+  mid=sym(1/2);
+  pts = sym([0,    0;
+             1,    0;
+             0,    1;
+             mid,  0;
+             mid,  mid;
+             0,  mid]);
+   f(x,y) = [1, x, y, x.*y, x.^2, y.^2];
+   XY = cell2sym(f(pts(:,1), pts(:,2)));
+   n = size(pts,1);
+   sf = sym(zeros(1,n));
+   for i=1:n
+      w = sym(zeros(n,1));
+      w(i) = 1;
+      sf(i) = dot(formula(f)', XY\w);
+   end
+end
 function [sf] = Quad9ShapeFun()
   syms x y z
   mid=sym(1/2);
