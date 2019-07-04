13b22be7ddbbc34932a34c9aa5e552d25cee445b
Roman Putanowicz <putanowr@gmail.com>
2019-07-04 16:50:07 +0200

    Added function to make boundary selector for flagshyp

---
+++ src/packages/+mp/+exports/+flagshyp/makeBcSelector.m
@@ -0,0 +1,12 @@
+function [selector] = makeBcSelector(bcRegistry)
+  % Return selector for boundary conditions. Selector is a structure
+  % that maps region names to numeric tags. The values of numeric tags
+  % depend on the boundary conditions specified on the regions.
+  rn = bcRegistry.regionNames();
+  selector = struct();
+  for regionName = rn
+    bc = bcRegistry.get(regionName{:},'Displacement');
+    selector.(regionName{:}) = mp.exports.flagshyp.bc2tag(bc.type);
+  end
+end
+
