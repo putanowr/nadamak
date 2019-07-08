5cc9f55a0b376d7192e6b0da3f55865732f276ae
Roman Putanowicz <putanowr@gmail.com>
2019-07-08 10:23:36 +0200

    Fixed call to BcFactory

--- src/tests/suites/flagshyp/test_bc2tag.m
+++ src/tests/suites/flagshyp/test_bc2tag.m
@@ -1,15 +1,15 @@
 mp_test_register('bc2tag', 'Test conversion from BcType to FlagSHyp tag');
 bc2tag = @mp.exports.flagshyp.bc2tag;
-var = 'Displacement'
-mp_test_assert_equal(0, bc2tag(mp.BcFactory('NotSet', var), 3));
-mp_test_assert_equal(1, bc2tag(mp.BcFactory('FixityX', var), 3));
-mp_test_assert_equal(2, bc2tag(mp.BcFactory('FixityY', var), 3));
-mp_test_assert_equal(3, bc2tag(mp.BcFactory('FixityXY', var), 3));
-mp_test_assert_equal(4, bc2tag(mp.BcFactory('FixityZ', var), 3));
-mp_test_assert_equal(5, bc2tag(mp.BcFactory('FixityXZ', var), 3));
-mp_test_assert_equal(6, bc2tag(mp.BcFactory('FixityYZ', var), 3));
-mp_test_assert_equal(7, bc2tag(mp.BcFactory('Fixity', var), 3));
+var = 'Displacement';
+mp_test_assert_equal(0, bc2tag(mp.BcFactory.produce('NotSet', var), 3));
+mp_test_assert_equal(1, bc2tag(mp.BcFactory.produce('FixityX', var), 3));
+mp_test_assert_equal(2, bc2tag(mp.BcFactory.produce('FixityY', var), 3));
+mp_test_assert_equal(3, bc2tag(mp.BcFactory.produce('FixityXY', var), 3));
+mp_test_assert_equal(4, bc2tag(mp.BcFactory.produce('FixityZ', var), 3));
+mp_test_assert_equal(5, bc2tag(mp.BcFactory.produce('FixityXZ', var), 3));
+mp_test_assert_equal(6, bc2tag(mp.BcFactory.produce('FixityYZ', var), 3));
+mp_test_assert_equal(7, bc2tag(mp.BcFactory.produce('Fixity', var), 3));
 bcval.value = [0,0,nan];
-mp_test_assert_equal(3, bc2tag(mp.BcFactory('Displacement', var, bcval), 3));
+mp_test_assert_equal(3, bc2tag(mp.BcFactory.produce('Displacement', var, bcval), 3));
 bcval.value = [0, nan];
-mp_test_assert_equal(1, bc2tag(mp.BcFactory('Displacement', var, bcval), 2));
+mp_test_assert_equal(1, bc2tag(mp.BcFactory.produce('Displacement', var, bcval), 2));
