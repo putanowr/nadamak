959c76632827da8e2cf9d26974a58b2152ae8e46
Roman Putanowicz <putanowr@gmail.com>
2019-07-09 10:49:53 +0200

    Implemented function writeLoadingData

--- src/packages/+mp/+exports/+flagshyp/writeLoadingData.m
+++ src/packages/+mp/+exports/+flagshyp/writeLoadingData.m
@@ -1,3 +1,9 @@
-function writeLoadingData(fid, mesh, bc)
-  fprintf(fid, 'Write loding data\n');
+function writeLoadingData(fid, mesh, bc, grav)
+  % Write loading data header
+  nlpds = mp.countPointLoadNodes(mesh, bc);
+  nprs = mp.countDisplacedDofs(mesh, bc);
+  nbpel = mp.countPressureElements(mesh, bc);
+  fprintf(fid, '%d %d %d ', nlpds, nprs, nbpel);
+  vecFormat.format = '%f';
+  mp_write_vector(fid, grav, mesh.ambientDim, vecFormat)
 end
