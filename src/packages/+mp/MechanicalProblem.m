cb99e22abb2f574161dcfb4d957a2ab21345b358
Roman Putanowicz <putanowr@gmail.com>
2019-07-09 10:45:06 +0200

    Added addGravity function

--- src/packages/+mp/MechanicalProblem.m
+++ src/packages/+mp/MechanicalProblem.m
@@ -1,5 +1,5 @@
-classdef MechanicalProblem < mp.Problem 
-  % Mechanical problem calss 
+classdef MechanicalProblem < mp.Problem
+  % Mechanical problem calss
   methods
     function [obj] = MechanicalProblem(geometry)
       obj = obj@mp.Problem(mp.ProblemType.Mechanical, geometry);
@@ -7,7 +7,20 @@ classdef MechanicalProblem < mp.Problem
     function setupVariables(obj)
       d = obj.geometry.dim;
       varname = 'Displacement';
-      obj.variables.(varname) = mp.Variable(varname, d, mp.VariableType.State); 
+      obj.addVariable(mp.Variable(varname, d, mp.VariableType.State));
+    end
+    function addGravity(obj, gravityVec)
+      % Adds 'gravity' parameter to the problem
+      d = obj.geometry.dim;
+      if nargin < 2
+        gravityVec(d) = -9.81;
+      end
+      if numel(gravityVec) ~= d
+        error('Inconsisten size for gravity vector: geom is %d-dim, vec is %d-dim', d, numel(gravityVec));
+      end
+      variable = mp.Variable('gravity', d, mp.VariableType.Data);
+      obj.myspace.addData('gravity', gravityVec(1:d));
+      obj.addVariable(variable);
     end
   end
 end
