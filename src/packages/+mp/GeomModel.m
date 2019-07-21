b33623ec4250a14967b40cef044d2e8189f486e1
Roman Putanowicz <putanowr@gmail.com>
2019-07-21 12:46:43 +0200

    Added method to retrun names of regions of given dimension

--- src/packages/+mp/GeomModel.m
+++ src/packages/+mp/GeomModel.m
@@ -38,6 +38,23 @@ classdef GeomModel < handle
       pth = split(cln, '.');
       name = mp.GeomFactory.projectName(pth{end});
     end
+    function [names] = regionsPerDim(obj, dim)
+      % Return cell array of strings of region names of specific
+      % dimension.
+      prefix = {{"p_", ["b_", "i_"], ["s_", "d_"], "v_"},...
+                {"p_", ["b_", "i_"], "s_", ["v_", "d_"]}};
+      if nargin < 2
+        names = obj.regions();
+      else
+        if isempty(dim) || ismissing(dim)
+          names = obj.regions();
+        else
+          allnames = obj.regions();
+          pr = prefix{obj.dim-1};
+          names = allnames(startsWith(allnames, pr{dim+1}));
+        end
+      end
+    end
   end
   methods (Abstract)
     [lc] = coarsest_lc(obj)
