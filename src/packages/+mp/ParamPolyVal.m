2fad1c23b1190512e9be327bf08b0ad100ddd3d7
Roman Putanowicz <putanowr@gmail.com>
2019-07-24 13:00:27 +0200

    Fixed methods for geting value if global time is outside value explicit scope

--- src/packages/+mp/ParamPolyVal.m
+++ src/packages/+mp/ParamPolyVal.m
@@ -14,7 +14,13 @@ classdef ParamPolyVal < mp.ParamValueBase
       obj.xv = [x,v];
     end
     function val = getValue(obj, t)
-      val = interp1(obj.xv(:,1),obj.xv(:,2:end),t); 
+      if t < obj.xv(1,1)
+        val = obj.xv(1,2:end);
+      elseif t > obj.xv(end,1)
+        val = obj.xv(end,2:end);
+      else        
+        val = interp1(obj.xv(:,1),obj.xv(:,2:end),t);
+      end  
     end
     function td = dataTable(obj,components)
       if nargin < 2
