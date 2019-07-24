f21c30635b6a1a2ace2b5150e66115e77c90a3ab
Roman Putanowicz <putanowr@gmail.com>
2019-07-24 18:50:18 +0200

    Added factory for Params

---
+++ src/packages/+mp/ParamFactory.m
@@ -0,0 +1,44 @@
+classdef ParamFactory < handle 
+  methods(Static)
+    function [pval] = produce(paramtype, params)
+      pt = mp.ParamType(paramtype);
+      fromfile=false;
+      if isfield(params, 'fid')
+        fid = params.fid;
+        fromfile=true;
+      elseif isfield(params, 'path')
+        fid = fopen(params.path, "r");
+        fromfile=true;
+      end
+      if fromfile
+        datasize = fscanf(fid, '%d', [1,2]);
+        data = fscanf(fid, '%f', datasize);
+        data = data';
+      else
+        data = mp_get_option(params, 'value', nan);
+        if ischar(data)
+          data = mp_parse_array(data);
+        end  
+        datasize = size(data);
+      end
+      if ismissing(data)
+        error('Missing value for param %s', char(paramtype));
+      end
+      switch(pt)
+        case mp.ParamType.Const
+          if datasize(1) ~= 1
+            error('Invalid data for ParamConstVal size=%d > 1', datasize(1));
+          end
+          pval = mp.ParamConstVal(data);
+        case mp.ParamType.Poly
+          if datasize(2) < 2 || datasize(1) < 2
+            error('Invalid data for ParamPolyVal size=[%d,%d]', datasize(1),...
+                  datasize(2));
+          end
+          pval = mp.ParamPolyVal(data(:,1), data(:,2:end));
+        otherwise
+          error('Unsupported parameter type %d', char(paramtype));
+      end    
+    end
+  end
+end
