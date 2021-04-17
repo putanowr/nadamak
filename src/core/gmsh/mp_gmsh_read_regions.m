a9241d0ae543eb766f7c4813ebbb267ca8822483
Roman Putanowicz <putanowr@gmail.com>
2021-04-17 10:07:04 +0200

    Added mockup of reading regions in GMSH file version 4

--- src/core/gmsh/mp_gmsh_read_regions.m
+++ src/core/gmsh/mp_gmsh_read_regions.m
@@ -1,5 +1,24 @@
 %% Read data into array of region structures. 
-function [regions] = mp_gmsh_read_regions(fid_or_name)
+function [regions] = mp_gmsh_read_regions(fid_or_name, version)
+  arguments
+      fid_or_name
+      version (1,1) uint32 = 4
+  end
+  if version == 4
+    elements = local_read_regions_ver_4(fid_or_name);
+  elseif version == 2
+    elements = local_read_regions_ver_2(fid_or_name);
+  else
+    error('Invalid msh version %d', version)
+  end
+end
+
+function [regions] = local_read_regions_ver_4(fid_or_name)
+  error('Not implemented yet')
+  regions = []
+end
+
+function [regions] = local_read_regions_ver_2(fid_or_name)
   [fid, needclose] = mp_get_fid(fid_or_name);
   mp_read_until_section(fid, '\$PhysicalNames');
   tline = fgetl(fid);
